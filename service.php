43dbae2967e8a0afc6da8c07e9600c95d3b99968
okovalov <oleksandr.kovalov@gmail.com>
2013-05-26 20:10:48 -0400

    Added server side WebService file

---
+++ service.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Url at what WebService will be called
+ *
+ * Copyright(c) 2013 Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
+ *
+ * MIT Licensed
+ */
+
+require_once('WebService.php');
+
+$webService = new WebService();
+
+$webService->runAction();
