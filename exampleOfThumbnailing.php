d94f1d45cc76f066bdf8d1cbab71b5ee253a4475
okovalov <oleksandr.kovalov@gmail.com>
2013-05-26 20:10:26 -0400

    Added examples of using WebService

---
+++ exampleOfThumbnailing.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Example of using WebService to get a resized file thumbnail
+ *
+ * Copyright(c) 2013 Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
+ *
+ * MIT Licensed
+ */
+
+// First of all check if curl is installed
+if ( ! function_exists('curl_init')){
+    die('Sorry cURL is not installed!');
+}
+
+// Define a file name here
+$fullFilePath = 'test.jpg';
+
+// Define a web service url
+$uploadUrl    = 'http://localhost/service.php';
+
+// Here a list of parameters with examples of values which needs to be sent into the web-service
+$postParameterArray = array(
+    'action'   => 'thumbnail',
+    'fileName' => "$fullFilePath",
+    'format'   => 'png',
+    'width'    => '300',
+    'height'   => '500'
+);
+
+// Curl routine
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $uploadUrl);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postParameterArray);
+
+$response = curl_exec($ch);
+
+// We need to know headers size to be able to split headers and body of the response
+$headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+
+// Close curl right away since we don't need that anymore
+curl_close($ch);
+
+// Now we're gotta split headers and body of the response
+$headerString = substr($response, 0, $headerSize);
+$body         = substr($response, $headerSize);
+$headerToken  = strtok($headerString, "\n\t");
+
+// Print the headers
+while ($headerToken !== false) {
+    header($headerToken);
+    $headerToken = strtok("\n\t");
+}
+
+// Print the body
+echo $body;
